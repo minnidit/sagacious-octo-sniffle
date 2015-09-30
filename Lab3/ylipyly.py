9db2679e1f58e68a8c19a790ddae1c8f275d7e5a
minnidit <minni.hiltunen@gmail.com>
2015-09-30 15:40:07 +0000

    instructions

--- Lab3/ylipyly.py
+++ Lab3/ylipyly.py
@@ -2,3 +2,12 @@
 
 print "aa"
 print "aaAAAAA"
+
+#Develop	a	short	program,	which	tests	to	see	if	a	string	input	on	the	command	line	is	a	
+#palindrome.	The	program	can	simply	print	out	“True”	or	“False”.	You	should	test	it	with	the	
+#following	strings.
+#a. Oxo
+#b. OXO
+#c. 123454321
+#d. ROTATOR
+#e. 12345	54321
